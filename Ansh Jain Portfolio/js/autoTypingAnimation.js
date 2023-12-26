ebba2c0966d0912684a6681b0726fa832351bf4f
jainansh2003 <149111119+jainansh2003@users.noreply.github.com>
2023-12-26 11:52:45 +0530

    Add files via upload

---
+++ Ansh Jain Portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ansh Jain</span> Third-year B.Tech student, possesses a strong passion for web development and a keen interest in machine learning.  With hands-on experience in designing and building web applications  My commitment to continuous learning and my aspiration to bridge the gap between web development and machine learning make me a motivatedand promising candidate for any forward-thinking organization."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
